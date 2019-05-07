9b568aac0634d91a99389948d15a3aee8afe749d
Tiago P. Casemiro <tiago.casemiro@m4u.com.br>
2019-05-07 18:51:05 -0300

    STN-001 Add gradle configure and domain classs

--- Project/domain/src/main/java/com/domain/Fact.kt
+++ Project/domain/src/main/java/com/domain/Fact.kt
@@ -1,8 +1,12 @@
 package com.domain
 
+import com.google.gson.annotations.SerializedName
+
 class Fact {
     var category: Category? = null
+    @SerializedName("icon_url")
     var icon: String? = null
+    @SerializedName("id")
     var hash: String? = null
     var url: String? = null
     var value: String? = null
--- Project/domain/src/main/java/com/domain/PastSearch.kt
+++ Project/domain/src/main/java/com/domain/PastSearch.kt
@@ -1,5 +1,11 @@
 package com.domain
 
+import android.arch.persistence.room.PrimaryKey
+import com.google.gson.annotations.Expose
+
 class PastSearch {
+    @Expose(serialize = false, deserialize = false)
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0
     var term: String? = null
 }
