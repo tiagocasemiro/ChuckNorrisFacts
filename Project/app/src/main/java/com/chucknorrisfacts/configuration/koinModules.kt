d101e49dbd2ad3d594f9dd00f7b5e4a0f27fbee6
Tiago P. Casemiro <tiago.casemiro@m4u.com.br>
2019-05-22 16:14:47 -0300

    STN-001 Add typ adapter deserializer on main module

--- Project/app/src/main/java/com/chucknorrisfacts/configuration/koinModules.kt
+++ Project/app/src/main/java/com/chucknorrisfacts/configuration/koinModules.kt
@@ -5,10 +5,14 @@ import com.chucknorrisfacts.BuildConfig
 import com.chucknorrisfacts.controller.SearchController
 import com.chucknorrisfacts.model.repository.local.ApplicationDatabase
 import com.chucknorrisfacts.model.repository.remote.ClientApi
+import com.chucknorrisfacts.model.repository.remote.FactDeserialize
+import com.domain.Fact
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module.module
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 val controllersModule = module {
@@ -35,6 +39,7 @@ val clientApiModule = module {
     factory {
         Retrofit.Builder()
             .baseUrl(BuildConfig.HOST)
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().registerTypeAdapter(Fact::class.java, FactDeserialize()).create()))
             .client(
                 OkHttpClient.Builder()
                     .connectTimeout(5000, TimeUnit.MILLISECONDS)
