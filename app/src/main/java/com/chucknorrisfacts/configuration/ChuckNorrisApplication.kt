3a6a2feb607021b5bed0ebe8d83f53a0ebc73f98
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-02-26 11:43:40 -0300

    Remove crashlitcs

--- app/src/main/java/com/chucknorrisfacts/configuration/ChuckNorrisApplication.kt
+++ app/src/main/java/com/chucknorrisfacts/configuration/ChuckNorrisApplication.kt
@@ -3,8 +3,6 @@ package com.chucknorrisfacts.configuration
 import android.app.Application
 import androidx.emoji.bundled.BundledEmojiCompatConfig
 import androidx.emoji.text.EmojiCompat
-import com.crashlytics.android.Crashlytics
-import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.android.startKoin
 
 
@@ -12,7 +10,6 @@ import org.koin.android.ext.android.startKoin
 class ChuckNorrisApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        Fabric.with(this, Crashlytics())
         startKoin(this, listOf(controllersModule, serviceModule, databaseModule, clientApiModule))
         EmojiCompat.init(BundledEmojiCompatConfig(this))
     }
